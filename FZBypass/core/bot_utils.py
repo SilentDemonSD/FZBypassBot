05bc2d860f37053cff5d5193148d1cbdcfe2b013
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-08-08 21:44:39 +0530

    shit

--- FZBypass/core/bot_utils.py
+++ FZBypass/core/bot_utils.py
@@ -4,8 +4,11 @@ async def auth_topic(_, message):
     for chat in Config.AUTH_CHATS:
         if ':' in chat:
             chat_id, topic_id = chat.split(':')
-            if message.chat.id == chat_id and (is_forum := message.reply_to_message) and not hasattr(is_forum, text) and topic_id == is_forum.id:
-            return True
+            if (message.chat.id == chat_id 
+                and (is_forum := message.reply_to_message) 
+                and not hasattr(is_forum, text) 
+                and topic_id == is_forum.id):
+                return True
         elif message.chat.id == chat:
             return True
     return False
