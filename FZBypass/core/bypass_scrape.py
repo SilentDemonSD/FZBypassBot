a9487711366a52db0112d9e7624a2288fba52efd
drxxstrange@gmail.com <drxxstrange@gmail.com>
2023-08-31 22:12:10 +0530

    easy filename & copy

--- FZBypass/core/bypass_scrape.py
+++ FZBypass/core/bypass_scrape.py
@@ -139,8 +139,9 @@ async def tamilmv(url):
     tor = soup.select('a[data-fileext="torrent"]')
     parse_data = f"<b><u>{soup.title.string}</u></b>"
     for no, (t, m) in enumerate(zip(tor, mag), start=1):
+        filename = sub(r"www\S+|\- |\.torrent", '', t.string)
         parse_data += f'''
         
-<b><i>{no}. {t.string.replace('.torrent', '')}</i></b>
-┖ <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>'''
+{no}. <code>{filename}</code>
+┖ <b>Links :</b> <a href="https://t.me/share/url?url={m['href'].split('&')[0]}"><b>Magnet </b>🧲</a>  | <a href="{t['href']}"><b>Torrent 🌐</b></a>'''
     return parse_data
